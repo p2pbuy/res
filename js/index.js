cf035515bc48aa487e5bb5922295dda23859be4e
p2pbuy <p2pbuy@p2pbuy.net>
2014-09-07 15:08:58 +0800

    开发
    新样式

--- js/index.js
+++ js/index.js
@@ -1,8 +1,9 @@
 $(function($){
 	//AJ 接口
 	var AJ = {
-			'createOrder' : '/order/buy',
-			'createOrderTest' : '/order/create',
+			//'createOrder' : '/order/buy',
+			//'createOrderTest' : '/order/create',
+			'createOrder' : '/order/create',
 			'amazon' : '/aj/third/amazon',
 			'amazonGetgoods' : '/aj/third/amazon/getgoods',
 	};
@@ -10,9 +11,10 @@ $(function($){
     var funcList = {
 		'inputCreator' : function ( data ) {
             var arr = [];
+            var objToStr;
             for ( var i in data ) {
                 if ( data.hasOwnProperty( i ) ) {
-                    arr[arr.length] = '<input type="hidden" name="' + i + '" value="' + data[i] + '" />';
+                	arr[arr.length] = '<input type="hidden" name="' + i + '" value="' + data[i] + '" />';
                 }
             }
             var randomKey = MAIN.getKey();
@@ -20,7 +22,7 @@ $(function($){
             return arr.join('');
         },
     	//发布需求
-    	'createOrder' : function ( e ) {
+    	/*'createOrder' : function ( e ) {
     		e.preventDefault();
     		var _thirdurl = $('[node-type=thirdurl]').val();
     		if(!_thirdurl){
@@ -43,14 +45,14 @@ $(function($){
         				var inputStr = funcList.inputCreator( json.data );
         				var formN = $('<form action="'+AJ.createOrder+'" method="post">'+ inputStr +'</form>').appendTo('body');
         				formN[0].submit();
-        				/*$.post( AJ.createOrder, {
+        				$.post( AJ.createOrder, {
         					title : json.data.title,
         					feature : json.data.feature,
         					price : json.data.price,
         					img : json.data.img,
         				}, function ( ) {
         					
-        				}, 'json' );*/
+        				}, 'json' );
         				return true;
         			}else{
         				alert('地址有问题，请重新填写');
@@ -59,9 +61,9 @@ $(function($){
         		}, 'json' );
     		}
     		return true;
-    	},
+    	},*/
     	//发布需求
-    	'createOrderTest' : function ( e ) {
+    	'createOrder' : function ( e ) {
     		e.preventDefault();
     		var _thirdurl = $('[node-type=thirdurl]').val();
     		if(!_thirdurl){
@@ -81,7 +83,7 @@ $(function($){
     			}, function ( json ) {
         			if( json.code == 100000 ) {
         				var inputStr = funcList.inputCreator( json.data );
-        				var formN = $('<form action="'+AJ.createOrderTest+'" method="post">'+ inputStr +'</form>').appendTo('body');
+        				var formN = $('<form action="'+AJ.createOrder+'" method="post">'+ inputStr +'</form>').appendTo('body');
         				formN[0].submit();
         			}else{
         				alert('地址有问题，请重新填写');
@@ -99,7 +101,7 @@ $(function($){
     
     var evtInit = function () {
     	$('#search_tool').delegate( '[action-type=create_order]', 'click', funcList.createOrder );
-    	$('#search_tool').delegate( '[action-type=create_order_test]', 'click', funcList.createOrderTest );
+    	//$('#search_tool').delegate( '[action-type=create_order_test]', 'click', funcList.createOrderTest );
     };
     // 执行初始化
     init();
