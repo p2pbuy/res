f96db0e13a2744bed302c70d2ddb35f6162b2edf
p2pbuy <p2pbuy@p2pbuy.net>
2014-11-20 11:33:03 +0800

    开发
    设置支付宝账号

--- js/user/member.js
+++ js/user/member.js
@@ -1,74 +1,70 @@
 $(function(){
-	//查看更多会员
-	var _page = 2;
-	$('[action-type=getmoreuserinfo]').click(function(){
-		var _count = 5;
-		
-		$.post('/managep2p/aj/member/getmoreuserinfojson',{
-			page : _page,
-			count : _count,
-		},function( json ){
-			if(json.code == 100000){
-				var _html = '';
-				obj = json.data;
-				for( var i in obj){
-					if(obj.hasOwnProperty(i)){
-						if(obj[i].hasOwnProperty('uid')){
-							var alipayusername;
-							if(obj[i]['extends']['alipayusername']){
-								alipayusername = obj[i]['extends']['alipayusername'];
-							}else{
-								alipayusername = '';
-							}
-							_html += '<li>';
-							_html += '<ul class="inline">';
-							_html += '<li class="a clearfix">';
-							_html += '<div class="detail">';
-							_html += '<div class="name"><em>uid：</em><span>' + obj[i]['uid'] + '</span></div>';
-							_html += '<div class="num"><em>nick：</em><span>' + obj[i]['nick'] + '</span></div>';
-							_html += '</div>';
-							_html += '</li>';
-							_html += '<li class="b"><input type="text" value="' + alipayusername + '" /></li>';
-							_html += '<li class="c">';
-							_html += '<small>';
-							_html += '</small>';
-							_html += '</li>';
-							_html += '<li class="d">';
-							_html += '<small>';
-							_html += '<a href="javascript:void(0);" class="btn btn-primary" action-type="setuserinfo">确认</a>';
-							_html += '</small>';
-							_html += '</li>';
-							_html += '</ul>';
-							_html += '</li>';
-						}
-					}
-				}
-				$('#allUserInfos').append(_html);
-				_page = _page + 1;
-			}
-		}, 'json' );
+	//搜索会员
+	$('[action-type=searchUser]').blur(function(){
+		searchUserInfo();
 		return true;
 	});
+	
+	function searchUserInfo(){
+		$.ajax({
+			url : "/managep2p/aj/member/getuserinfobyemailjson?email=" + $('[action-type=searchUser]').val(),
+			cache : false,
+			success: function(json){
+				json = eval('(' + json + ')');;
+				if(json.code == 100000){
+					var _html = '';
+					obj = json.data;
+					
+					var alipayusername = obj['extends']['alipayusername'];
+					if(alipayusername == null){
+						alipayusername = '';
+					}
+					
+					_html += '<li id="userInfoBox">';
+					_html += '<ul class="inline">';
+					_html += '<li class="a clearfix">';
+					_html += '<div class="detail">';
+					_html += '<div class="name"><em>uid：</em><span>' + obj['uid'] + '</span></div>';
+					_html += '<div class="num"><em>nick：</em><span>' + obj['nick'] + '</span></div>';
+					_html += '</div>';
+					_html += '</li>';
+					_html += '<li class="b"><input type="text" value="' + alipayusername + '" node-type="alipayusername"/></li>';
+					_html += '<li class="c">';
+					_html += '<small>';
+					_html += '</small>';
+					_html += '</li>';
+					_html += '<li class="d">';
+					_html += '<small>';
+					_html += '<a href="javascript:void(0);" class="btn btn-primary" action-type="setuserinfo" action-data="uid=' + obj['uid'] + '">确认</a>';
+					_html += '</small>';
+					_html += '</li>';
+					_html += '</ul>';
+					_html += '</li>';
+					
+					$('#allUserInfos').html(_html);
+				}
+			}
+		});
+		return true;
+	}
 	// 处理方法列表
     var funcList = {
     		//设置用户信息
     		'setuserinfo' : function ( e ) {
     			var el = $(e.currentTarget);
-    			var alipayusername = el.closest('#userInfoBox').find('[node-type=alipayusername]').val();
+    			var _alipayusername = el.closest('#userInfoBox').find('[node-type=alipayusername]').val();
     			var result = MAIN.getData(el,'action-data');
     			
-    			$.post('/aj/order/publicorder',{
-    				boid : result.boid,
-    				isshow : result.isshow,
+    			$.post('/managep2p/aj/member/setuserinfo',{
+    				uid : result.uid,
+    				type : 2,
+    				alipayusername : _alipayusername,
     			},function( json ){
     				if(json.code == 100000){
-    					if(result.isshow == 1){
-    						el.html('关闭');
-    						el.attr("action-data","boid=" + result.boid + "&isshow=0");
-    					}else{
-    						el.attr("action-data","boid=" + result.boid + "&isshow=1");
-    						el.html('开放');
-    					}
+    					alert('设置成功');
+    					searchUserInfo();
+    				}else{
+    					alert('设置失败');
     				}
     			}, 'json' );
     			
